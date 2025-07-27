e5beda3364c379dcb442d52687990244d7fc428d
iamakbarsha1 <iamakbarsha1@gmail.com>
2025-07-27 21:32:40 +0530

    completed functions ex

--- exercise/b_functions/src/main.rs
+++ exercise/b_functions/src/main.rs
@@ -4,15 +4,15 @@
 fn main() {
     let width = 4;
     let height = 7;
-    let depth = 10;
+    // let depth = 10;
+    let depth = 10.4555_f64;
     // 1. Try running this code with `cargo run` and take a look at the error.
     //
     // See if you can fix the error. It is right around here, somewhere.  If you succeed, then
     // doing `cargo run` should succeed and print something out.
-    {
-        let area = area_of(width, height);
-    }
-    println!("Area is {}", area);
+
+    let area = area_of(width, height);
+    println!("Area is {area}");
 
     // 2. The area that was calculated is not correct! Go fix the area_of() function below, then run
     //    the code again and make sure it worked (you should get an area of 28).
@@ -25,16 +25,25 @@ fn main() {
     //
     // If you get stuck, remember that this is *very* similar to what `area_of` does.
     //
-    //println!("Volume is {}", volume(width, height, depth));
+    println!("Volume is {}", volume(width, height, depth));
 }
 
 fn area_of(x: i32, y: i32) -> i32 {
     // 2a. Fix this function to correctly compute the area of a rectangle given
     // dimensions x and y by multiplying x and y and returning the result.
     //
-    return 0;
+    // return x * y;
+    x * y // tail expression
+
     // Challenge: It isn't idiomatic (the normal way a Rust programmer would do things) to use
     //            `return` on the last line of a function. Change the last line to be a
     //            "tail expression" that returns a value without using `return`.
     //            Hint: `cargo clippy` will warn you about this exact thing.
+}
+
+// fn volume(width: i32, height: i32, depth: i32) -> f64 {
+fn volume(width: i32, height: i32, depth: f64) -> f64 {
+    // width * height * depth
+    (width * height) as f64 * depth
+    // Reason for cast: width * height is i32; must convert to f64 to multiply with depth
 }
